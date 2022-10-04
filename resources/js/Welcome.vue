1c33e82b462ac355335d0da4aad1fb8305d61868
safiull <safiullalam9931@gmail.com>
2022-10-04 11:19:33 +0600

    Post crud.

---
+++ resources/js/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello world.</h1>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log("Ok")
+        console.log("Ok")
+        console.log("Ok")
+        console.log("Ok")
+    }
+}
+</script>
+
+<style>
+
+</style>
